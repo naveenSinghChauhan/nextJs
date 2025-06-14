3e113698deeb26880075e6b27cdc659db94725cb
amitKumar1510 <amtk2386188@gmail.com>
2025-06-14 19:55:16 +0530

    testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Banner from "./components/banner";
 import Footer from "./components/footer";
-
+import TestimonialCarousel from "./components/testimonial";
 
 export default function Home() {
   return (
 <>
 <Banner />
+<TestimonialCarousel />
 <Footer />
 </>
   );
